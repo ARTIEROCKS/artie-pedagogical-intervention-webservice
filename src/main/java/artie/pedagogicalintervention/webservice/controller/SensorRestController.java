aa5c8b29de74c07c702ce7fa83df64f8bdf980af
Luis-Eduardo Imbernon <leimbernon@ai-mind.com>
2020-01-19 20:53:00 +0100

    Added the function to send the sensor data through the pedagogical intervention web service.

--- src/main/java/artie/pedagogicalintervention/webservice/controller/SensorRestController.java
+++ src/main/java/artie/pedagogicalintervention/webservice/controller/SensorRestController.java
@@ -1,21 +1,19 @@
 package artie.pedagogicalintervention.webservice.controller;
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import artie.common.web.dto.Student;
+import artie.common.web.dto.SecuritySensorData;
 import artie.pedagogicalintervention.webservice.service.SecurityService;
 import artie.pedagogicalintervention.webservice.service.SensorService;
-import artie.sensor.common.dto.SensorObject;
 
 @Controller
 @RestController
@@ -36,19 +34,16 @@ public class SensorRestController {
 	 * @param user
 	 * @param password
 	 */
-	@GetMapping(path = "/sendSensorData",
+	@PostMapping(path = "/sendSensorData",
 	         produces = {MediaType.APPLICATION_JSON_VALUE})
 	@ResponseStatus(HttpStatus.FOUND)
-	public void sendSensorData(@RequestParam("data") List<SensorObject> data, 
-							   @RequestParam("student") Student student, 
-							   @RequestParam("userName") String user, 
-							   @RequestParam("password") String password) {
+	public void sendSensorData(@RequestBody SecuritySensorData securitySensorData) {
 		
 		//1- Login into the system
-		if(securityService.login(user, password)) {
+		if(securityService.login(securitySensorData.getUser(), securitySensorData.getPassword())) {
 			
 			//2- Getting the emotional state
-			this.sensorService.getEmotionalState(data, student, user, password);
+			this.sensorService.getEmotionalState(securitySensorData.getData(), securitySensorData.getStudent(), securitySensorData.getUser(), securitySensorData.getPassword());
 			
 		}
 		
