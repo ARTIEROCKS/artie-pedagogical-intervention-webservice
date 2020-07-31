fb7fa9e3fbd2b605858c4513ce996eb6edc2e816
Luis-Eduardo Imbernon <leimbernon@ai-mind.com>
2020-07-31 23:06:36 +0200

    Added the CORS header.

--- src/main/java/artie/pedagogicalintervention/webservice/controller/PedagogicalSoftwareRestController.java
+++ src/main/java/artie/pedagogicalintervention/webservice/controller/PedagogicalSoftwareRestController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import artie.pedagogicalintervention.webservice.service.PedagogicalSoftwareServi
 @Controller
 @RestController
 @RequestMapping("/api/v1/pedagogicalsoftware")
+@CrossOrigin(origins="*", allowedHeaders="*")
 public class PedagogicalSoftwareRestController {
 
 	@Autowired
