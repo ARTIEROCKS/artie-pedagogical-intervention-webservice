4cda5c29574ad749b6a8ea25f6d571e7a6ed633c
Luis-Eduardo Imbernon <leimbernon@ai-mind.com>
2020-10-06 21:18:26 +0200

    Added the userId to the exercise model

--- src/main/java/artie/pedagogicalintervention/webservice/model/PedagogicalSoftwareSolution.java
+++ src/main/java/artie/pedagogicalintervention/webservice/model/PedagogicalSoftwareSolution.java
@@ -12,6 +12,7 @@ public class PedagogicalSoftwareSolution {
 	
 	@Id
 	private String id;
+	private String userId;
 	private String exercise;
 	private List<PedagogicalSoftwareElement> elements = new ArrayList<>();
 	
@@ -20,6 +21,12 @@ public class PedagogicalSoftwareSolution {
 	}
 	public void setId(String id) {
 		this.id = id;
+	}
+	public String getUserId() {
+		return this.userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
 	}
 	public String getExercise() {
 		return this.exercise;
