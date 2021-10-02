29089e9bba68931d674cdc9c6526ea47b70233d5
Luis Eduardo Imbernón <imbernon@gmail.com>
2021-10-02 19:33:16 +0200

    artie_pedagogical_intervention_webservice-36: CORS deleted.

--- src/main/java/artie/pedagogicalintervention/webservice/controller/SensorRestController.java
+++ src/main/java/artie/pedagogicalintervention/webservice/controller/SensorRestController.java
@@ -5,11 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import artie.common.web.dto.SecuritySensorData;
 import artie.pedagogicalintervention.webservice.service.SecurityService;
@@ -18,6 +14,7 @@ import artie.pedagogicalintervention.webservice.service.SensorService;
 @Controller
 @RestController
 @RequestMapping("/api/v1/sensor")
+@CrossOrigin(origins="*", allowedHeaders="*")
 public class SensorRestController {
 	
 	@Autowired
