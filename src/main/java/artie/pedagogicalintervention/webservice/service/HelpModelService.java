80c48b9e64f8c3a768fd28098f102fe69f3b3f8a
Luis Eduardo Imbernón <imbernon@gmail.com>
2022-03-13 21:11:54 +0100

    Capturing some errors.

--- src/main/java/artie/pedagogicalintervention/webservice/service/HelpModelService.java
+++ src/main/java/artie/pedagogicalintervention/webservice/service/HelpModelService.java
@@ -49,29 +49,33 @@ public class HelpModelService {
      */
     public boolean predict(PedagogicalSoftwareData pedagogicalSoftwareData) {
 
-        //1- Gets the parent of the pedagogical software data, that will be sent
-        SoftwareData softwareData = pedagogicalSoftwareData.toDTO();
+        boolean result = false;
 
-        //2- Calls for the webservice
-        String wsResponse = restTemplate.postForObject(helpWebserviceUrl + "/predict", softwareData, String.class);
-
-        //3- Transforms the string into the object
-        Response response = null;
         try {
-            response = new ObjectMapper().readValue(wsResponse, Response.class);
+            //1- Gets the parent of the pedagogical software data, that will be sent
+            SoftwareData softwareData = pedagogicalSoftwareData.toDTO();
+
+            //2- Calls for the webservice
+            String wsResponse = restTemplate.postForObject(helpWebserviceUrl + "/predict", softwareData, String.class);
+
+            //3- Transforms the string into the object
+            Response response = new ObjectMapper().readValue(wsResponse, Response.class);
 
             //If there are an error in the response, we print the error
             if(response != null && response.getBody() != null && response.getBody().getMessage() == ResponseCodeEnum.ERROR.toString()){
                 System.out.println((String)response.getBody().getObject());
             }
-        } catch (JsonProcessingException e) {
+
+            result = response != null &&
+                    response.getBody().getMessage() != ResponseCodeEnum.ERROR.toString() &&
+                    response.getBody().getObject() != null ? ((int)response.getBody().getObject() == 1 ? true : false ): false;
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
         //3- Returns the boolean body object
-        return response != null &&
-                response.getBody().getMessage() != ResponseCodeEnum.ERROR.toString() &&
-                response.getBody().getObject() != null ? ((int)response.getBody().getObject() == 1 ? true : false ): false;
+        return result;
     }
 
 }
