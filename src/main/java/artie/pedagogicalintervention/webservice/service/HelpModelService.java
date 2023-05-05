f090f6d2a6172503fc1a7edbc1bbe5c49842c3f1
Luis Eduardo Imbernón <imbernon@gmail.com>
2023-05-05 18:31:57 +0200

    We add the sensor data in a rabbitmq server instead sending it to the database. This way we can separate better functional data.

--- src/main/java/artie/pedagogicalintervention/webservice/service/HelpModelService.java
+++ src/main/java/artie/pedagogicalintervention/webservice/service/HelpModelService.java
@@ -1,11 +1,9 @@
 package artie.pedagogicalintervention.webservice.service;
 
 import artie.common.web.dto.Response;
-import artie.common.web.dto.ResponseBody;
 import artie.common.web.dto.SoftwareData;
 import artie.common.web.enums.ResponseCodeEnum;
 import artie.pedagogicalintervention.webservice.model.PedagogicalSoftwareData;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
