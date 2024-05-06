87274b7ae8e081735d0e2dc9bb4142cb3f1cbd53
Luis Eduardo Imbernón <imbernon@gmail.com>
2024-05-06 21:12:28 +0200

    Added pagination to the batch

--- src/main/java/artie/pedagogicalintervention/webservice/batch/BatchDistanceService.java
+++ src/main/java/artie/pedagogicalintervention/webservice/batch/BatchDistanceService.java
@@ -9,6 +9,9 @@ import artie.pedagogicalintervention.webservice.service.PedagogicalSoftwareSolut
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -42,114 +45,127 @@ public class BatchDistanceService {
         }
 
         //Getting all the pedagogical software data elements
-        List<PedagogicalSoftwareData> elements = softwareService.findAll();
-        log.info("Found " +elements.size() + " elements");
+        int pageNumber = 0;
+        final int PAGE_SIZE = 100;
+        Page<PedagogicalSoftwareData> page = null;
+        Pageable pageable = PageRequest.of(pageNumber, PAGE_SIZE);
 
-        for(PedagogicalSoftwareData psd: elements) {
-            log.info("Starting the process of Pedagogical Software Data id " + psd.getId());
+        // Walking through the pages
+        do {
+            page = softwareService.findAllPaginated(pageable);
+            log.info("Found {} elements", page.getTotalElements());
+            for (PedagogicalSoftwareData psd : page.getContent()) {
+                log.info("Starting the process of Pedagogical Software Data id " + psd.getId());
 
-            //We get all the possible solutions in the database for this exercise
-            if (psd.getExercise() != null && psd.getStudent() != null) {
-                log.trace("Getting all the possible solutions for the exercise id (" + psd.getExercise().getId() + ") and the student id (" + psd.getStudent().getId() + ")");
-                solutions = solutionService.findByExerciseAndUserId(psd.getExercise(), psd.getStudent().getUserId());
-            } else {
-                solutions = new ArrayList<>();
-            }
-
-            //Calculates ARTIE distances between the pedagogical software data and the different solutions
-            if (psd.getExercise() != null && psd.getStudent() != null) {
-                log.info("Calculating ARTIE distances. Solutions found: " + solutions.size() + " for exercise id (" + psd.getExercise().getId() + ") and the student id (" + psd.getStudent().getId() + ")");
-            }
-            PedagogicalSoftwareSolution bestSolution = null;
-            SolutionDistance bestDistance = null;
-            SolutionDistance currentDistance;
-
-            PedagogicalSoftwareSolution bestTreeSolution = null;
-            double bestTreeDistance = -1;
-            double currentTreeDistance = 0;
-
-            for (PedagogicalSoftwareSolution solution : solutions) {
-
-                //Gets the best solution in base of the ARTIE distance
-                currentDistance = distanceCalculationService.distanceCalculation(psd, solution);
-                if (bestDistance == null || bestDistance.getTotalDistance() > currentDistance.getTotalDistance()) {
-                    bestDistance = currentDistance;
-                    bestSolution = solution;
+                //We get all the possible solutions in the database for this exercise
+                if (psd.getExercise() != null && psd.getStudent() != null) {
+                    log.trace("Getting all the possible solutions for the exercise id (" + psd.getExercise().getId() + ") and the student id (" + psd.getStudent().getId() + ")");
+                    solutions = solutionService.findByExerciseAndUserId(psd.getExercise(), psd.getStudent().getUserId());
+                } else {
+                    solutions = new ArrayList<>();
                 }
 
-                //Gets the best solution in base of the tree distance
-                currentTreeDistance = distanceCalculationService.aptedDistanceCalculation(psd.toString(), solution.toString());
-                if (bestTreeDistance == -1 || bestTreeDistance > currentTreeDistance) {
-                    bestTreeDistance = currentTreeDistance;
-                    bestTreeSolution = solution;
+                //Calculates ARTIE distances between the pedagogical software data and the different solutions
+                if (psd.getExercise() != null && psd.getStudent() != null) {
+                    log.info("Calculating ARTIE distances. Solutions found: " + solutions.size() + " for exercise id (" + psd.getExercise().getId() + ") and the student id (" + psd.getStudent().getId() + ")");
                 }
+                PedagogicalSoftwareSolution bestSolution = null;
+                SolutionDistance bestDistance = null;
+                SolutionDistance currentDistance;
+
+                PedagogicalSoftwareSolution bestTreeSolution = null;
+                double bestTreeDistance = -1;
+                double currentTreeDistance = 0;
+
+                for (PedagogicalSoftwareSolution solution : solutions) {
+
+                    //Gets the best solution in base of the ARTIE distance
+                    currentDistance = distanceCalculationService.distanceCalculation(psd, solution);
+                    if (bestDistance == null || bestDistance.getTotalDistance() > currentDistance.getTotalDistance()) {
+                        bestDistance = currentDistance;
+                        bestSolution = solution;
+                    }
+
+                    //Gets the best solution in base of the tree distance
+                    currentTreeDistance = distanceCalculationService.aptedDistanceCalculation(psd.toString(), solution.toString());
+                    if (bestTreeDistance == -1 || bestTreeDistance > currentTreeDistance) {
+                        bestTreeDistance = currentTreeDistance;
+                        bestTreeSolution = solution;
+                    }
+                }
+
+                if (bestDistance != null && psd.getSolutionDistance() != null) {
+                    log.trace("Old ARTIE Distance: " + psd.getSolutionDistance().getTotalDistance() + " - New ARTIE Distance: " + bestDistance.getTotalDistance());
+                } else {
+                    log.error("ARTIE Distance is NULL");
+                }
+
+                //Calculates the APTED distances with respect the best solution
+                if (psd.getExercise() != null && psd.getStudent() != null) {
+                    log.info("Calculating APTED distances. Solutions found: " + solutions.size() + " for exercise id (" + psd.getExercise().getId() + ") and the student id (" + psd.getStudent().getId() + ")");
+                }
+                SolutionDistance maximumDistance = null;
+                double maximumTreeDistance = 0.0;
+                double aptedDistance = 0.0;
+                String tree = psd.toString();
+                String solutionTree = "";
+
+                //Calculating the ARTIE distance with respect the best solution get by the same method
+                if (bestSolution != null) {
+                    maximumDistance = distanceCalculationService.distanceCalculation(new PedagogicalSoftwareData(), bestSolution);
+                }
+
+                //Calculating the APTED distance with respect the best solution get by the same method
+                if (bestTreeSolution != null) {
+                    solutionTree = bestTreeSolution.toString();
+                    maximumTreeDistance = distanceCalculationService.aptedDistanceCalculation("{}", solutionTree);
+                    aptedDistance = distanceCalculationService.aptedDistanceCalculation(tree, solutionTree);
+                }
+                log.info("Old APTED Distance: " + psd.getAptedDistance() + " - New APTED Distance: " + aptedDistance);
+
+                //Calculates the grades
+                double artieGrade = 0.0;
+                double aptedGrade = 0.0;
+                if (maximumDistance != null && bestDistance != null) {
+                    artieGrade = softwareService.calculateGrade(maximumDistance.getTotalDistance(), bestDistance.getTotalDistance(), 10);
+                    log.info("Old ARTIE Grade: " + psd.getGrade() + " - New ARTIE Grade: " + artieGrade);
+                }
+                aptedGrade = softwareService.calculateGrade(maximumTreeDistance, aptedDistance, 10);
+                log.trace("Old Tree Grade: " + psd.getTreeGrade() + " - New Tree Grade: " + aptedGrade);
+
+                //Sets all the information within the distance object
+                psd.setRelatedSolution(bestSolution);
+                if (maximumDistance != null) {
+                    psd.setMaximumDistance(maximumDistance.getTotalDistance());
+                }
+                if (bestDistance != null) {
+                    psd.setSolutionDistance(bestDistance);
+                }
+                psd.setGrade(artieGrade);
+
+                psd.setTree(tree);
+                log.trace("Old Tree: " + psd.getTree() + " - New Tree: " + tree);
+                psd.setSolutionTree(solutionTree);
+                log.trace("Old Solution Tree: " + psd.getSolutionTree() + " - New Solution Tree: " + solutionTree);
+                psd.setMaximumTreeDistance(maximumTreeDistance);
+                psd.setAptedDistance(aptedDistance);
+                psd.setTreeGrade(aptedGrade);
+
+                //Transforms the pedagogical software data in the record
+                log.info("Finished the process of Pedagogical Software Data id " + psd.getId());
+
+                //Updates the data
+                log.info("Updating the element");
+                softwareService.update(psd);
+                log.info("Element updated");
             }
 
-            if (bestDistance != null && psd.getSolutionDistance() != null) {
-                log.trace("Old ARTIE Distance: " + psd.getSolutionDistance().getTotalDistance() + " - New ARTIE Distance: " + bestDistance.getTotalDistance());
-            } else {
-                log.error("ARTIE Distance is NULL");
-            }
+            //Next page
+            pageNumber +=1;
+            pageable = page.nextPageable();
+            log.info("Paginating...{} of {}", pageNumber + 1, page.getTotalPages());
+        } while (!page.isLast());
 
-            //Calculates the APTED distances with respect the best solution
-            if (psd.getExercise() != null && psd.getStudent() != null) {
-                log.info("Calculating APTED distances. Solutions found: " + solutions.size() + " for exercise id (" + psd.getExercise().getId() + ") and the student id (" + psd.getStudent().getId() + ")");
-            }
-            SolutionDistance maximumDistance = null;
-            double maximumTreeDistance = 0.0;
-            double aptedDistance = 0.0;
-            String tree = psd.toString();
-            String solutionTree = "";
-
-            //Calculating the ARTIE distance with respect the best solution get by the same method
-            if (bestSolution != null) {
-                maximumDistance = distanceCalculationService.distanceCalculation(new PedagogicalSoftwareData(), bestSolution);
-            }
-
-            //Calculating the APTED distance with respect the best solution get by the same method
-            if (bestTreeSolution != null) {
-                solutionTree = bestTreeSolution.toString();
-                maximumTreeDistance = distanceCalculationService.aptedDistanceCalculation("{}", solutionTree);
-                aptedDistance = distanceCalculationService.aptedDistanceCalculation(tree, solutionTree);
-            }
-            log.info("Old APTED Distance: " + psd.getAptedDistance() + " - New APTED Distance: " + aptedDistance);
-
-            //Calculates the grades
-            double artieGrade = 0.0;
-            double aptedGrade = 0.0;
-            if (maximumDistance != null && bestDistance != null) {
-                artieGrade = softwareService.calculateGrade(maximumDistance.getTotalDistance(), bestDistance.getTotalDistance(), 10);
-                log.info("Old ARTIE Grade: " + psd.getGrade() + " - New ARTIE Grade: " + artieGrade);
-            }
-            aptedGrade = softwareService.calculateGrade(maximumTreeDistance, aptedDistance, 10);
-            log.trace("Old Tree Grade: " + psd.getTreeGrade() + " - New Tree Grade: " + aptedGrade);
-
-            //Sets all the information within the distance object
-            psd.setRelatedSolution(bestSolution);
-            if (maximumDistance != null) {
-                psd.setMaximumDistance(maximumDistance.getTotalDistance());
-            }
-            if (bestDistance != null) {
-                psd.setSolutionDistance(bestDistance);
-            }
-            psd.setGrade(artieGrade);
-
-            psd.setTree(tree);
-            log.trace("Old Tree: " + psd.getTree() + " - New Tree: " + tree);
-            psd.setSolutionTree(solutionTree);
-            log.trace("Old Solution Tree: " + psd.getSolutionTree() + " - New Solution Tree: " + solutionTree);
-            psd.setMaximumTreeDistance(maximumTreeDistance);
-            psd.setAptedDistance(aptedDistance);
-            psd.setTreeGrade(aptedGrade);
-
-            //Transforms the pedagogical software data in the record
-            log.info("Finished the process of Pedagogical Software Data id " + psd.getId());
-
-            //Updating the element
-            log.info("Updating the element");
-            softwareService.update(psd);
-            log.info("Updating successful");
-        }
-        log.info("Updating process finished");
+        log.info("PROCESS FINISHED!");
     }
 }
