bbcb9ec0f28f5d953d8c2151745c0704b6186654
Luis Eduardo Imbernón <imbernon@gmail.com>
2024-07-11 18:29:30 +0200

    Added the subscription to the new message queue.

---
+++ src/main/java/artie/pedagogicalintervention/webservice/dto/MessageDTO.java
@@ -0,0 +1,14 @@
+package artie.pedagogicalintervention.webservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MessageDTO {
+    private String userId;
+    private String contextId;
+    private String message;
+}
--- src/main/java/artie/pedagogicalintervention/webservice/service/ChatClientService.java
+++ src/main/java/artie/pedagogicalintervention/webservice/service/ChatClientService.java
@@ -1,9 +1,16 @@
 package artie.pedagogicalintervention.webservice.service;
 
+import artie.pedagogicalintervention.webservice.dto.MessageDTO;
 import chat.ChatGrpc.ChatBlockingStub;
 import chat.ChatOuterClass.ChatRequest;
 import chat.ChatOuterClass.ChatResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import net.devh.boot.grpc.client.inject.GrpcClient;
+import org.slf4j.Logger;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +18,16 @@ public class ChatClientService {
 
     @GrpcClient("chatService")
     private ChatBlockingStub chatBlockingStub;
+
+    @Value("${artie.webservices.conversations.queue}")
+    private String queue;
+    private final ObjectMapper objectMapper;
+    private Logger logger;
+
+    @Autowired
+    public ChatClientService(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
 
     public String getResponse(String userId, String contextId, String message) {
         ChatRequest request = ChatRequest.newBuilder()
@@ -21,5 +38,25 @@ public class ChatClientService {
 
         ChatResponse response = chatBlockingStub.getResponse(request);
         return response.getReply();
+    }
+
+    @RabbitListener(queues = "${artie.webservices.conversations.queue}")
+    public void receiveMessage(String messageContent) {
+        try {
+            // Parse the message content
+            MessageDTO message = objectMapper.readValue(messageContent, MessageDTO.class);
+
+            // Gets the answer from the chat
+            String reply = getResponse(message.getUserId(), message.getContextId(), message.getMessage());
+
+            // Log or handle the reply as needed
+            logger.info("Reply: " + reply);
+
+            //TODO: Add the logic to perform a pedagogical intervention
+
+        } catch (Exception e) {
+            // Handle exception
+            logger.error("Failed to process message: " + e.getMessage());
+        }
     }
 }
